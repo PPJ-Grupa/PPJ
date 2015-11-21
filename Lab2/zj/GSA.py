e5b118a7e415c538f8f8bc69a4e320570d8577dd
Zvonimir Jurelinac <zjurelinac@gmail.com>
2015-11-21 19:31:31 +0100

    Saving local - again

--- Lab2/zj/GSA.py
+++ Lab2/zj/GSA.py
@@ -3,22 +3,45 @@ import sys
 
 from collections import defaultdict
 from copy import copy
+from pprint import pprint
 
-# def calculateFirst( start, current, direct_first, grammar, ancestors ):
-#     if current == '$': return
-#     if current[ 0 ] != '<':
-#         direct_first[ start ].add( current )
-#
-#     for rule in grammar[ current ]:
-#         if rule[ 0 ] not in ancestors:
-#             calculateFirst( start, rule[ 0 ], direct_first, grammar, ancestors | { current } )
 
-def sequence_starts_with( seq, starts_with, empty_symbols ):
+# Global variables
+grammar         = defaultdict( list )
+rules           = []
+empty_symbols   = set()
+nonterminals    = []
+terminals       = []
+symbols         = []
+syncs           = []
+starts_with     = {}
+directly_starts_with = {}
+
+# Calculates a closure of an item set
+def closure( item_set ):
+    while( True ):
+        new_set = copy( item_set )
+        for item in item_set:
+            num, pos, lookahead = item
+            rule = rules[ num ][ 1 ]
+            if pos >= len( rule ) or rule[ pos ][ 0 ] != '<': continue
+            # print( rule_num, position, own_rule, symbol )
+            for index in grammar[ rule[ pos ] ]:
+                new_set.add( ( index, 0, 'x' ) )
+        if len( item_set ) == len( new_set ): break
+        item_set = new_set
+    return item_set
+
+# Returns a set of all the symbols a given sequence can start with
+def sequence_starts_with( seq ):
+    global empty_symbols
     result = set()
     for symbol in sequence:
         result |= starts_with[ symbol ]
         if symbol not in empty_symbols: break
     return result
+
+
 
 if __name__ == "__main__":
     lines = sys.stdin.readlines()
@@ -28,46 +51,34 @@ if __name__ == "__main__":
     terminals       = next( lines_iter ).split()[ 1: ]
     syncs           = next( lines_iter ).split()[ 1: ]
 
-    grammar     = defaultdict( list )
-    rules_list  = []
-    rules       = []
-    left_sym    = None
+    left_sym        = None
+    rule_count      = 1
 
-    try:
-        while True:
-            line = next( lines_iter )
-            if line[ 0 ] == ' ':
-                rules.append( line.split() )
-                rules_list.append( ( left_sym, line.split() ) )
-            else:
-                if left_sym is not None:
-                    grammar[ left_sym ] += rules
-
-                left_sym = line.strip()
-                rules = []
-
-    except StopIteration:
-        grammar[ left_sym ] += rules
-
-    grammar[ '<%>' ] = [ [ nonterminals[ 0 ] ] ]
-    rules_list.append( ( '<%>', [ nonterminals[ 0 ] ] ) )
+    grammar[ '<%>' ] = [ 0 ]
+    rules = [ ( '<%>', [ nonterminals[ 0 ] ] ) ]
     nonterminals.insert( 0, '<%>' )
     symbols = nonterminals + terminals
 
-    directly_starts_with = { symbol : { symbol } for symbol in symbols }
-    empty_symbols = set()
+    for line in lines[ 3: ]:
+        if line[ 0 ] == ' ':
+            grammar[ left_sym ].append( rule_count )
+            rules.append( ( left_sym, line.split() ) )
+            rule_count += 1
+        else:
+            left_sym = line.strip()
 
-    for left, right in rules_list:
+    pprint( rules )
+    # pprint( grammar )
+
+    directly_starts_with = { symbol : { symbol } for symbol in symbols }
+
+    for left, right in rules:
         if right[ 0 ] == '$':
             empty_symbols.add( left )
 
-    # if len( empty_symbols ) > 0: terminals.append( '$' )
-
-    # print( all_rules )
-
     while True:
         new_empty_symbols = copy( empty_symbols )
-        for left_sym, rule in rules_list:
+        for left_sym, rule in rules:
             is_empty = True
             for symbol in rule:
                 if symbol not in empty_symbols:
@@ -79,15 +90,11 @@ if __name__ == "__main__":
             break
         empty_symbols = new_empty_symbols
 
-    print( empty_symbols )
-
-    for left, right in rules_list:
+    for left, right in rules:
         if right[ 0 ] != '$':
             for symbol in right:
                 directly_starts_with[ left ].add( symbol )
                 if symbol not in empty_symbols: break
-
-    print( directly_starts_with )
 
     starts_with = copy( directly_starts_with )
 
@@ -96,38 +103,15 @@ if __name__ == "__main__":
             if sym2 in starts_with[ sym1 ]:
                 starts_with[ sym1 ] |= starts_with[ sym2 ]
 
-    print( starts_with )
+    # print( rules_list )
 
+    # for i, rule in enumerate( rules_list ):
+    #     lhs, rhs = rule
+    #     print( i, lhs, '->', ' '.join( rhs ) )
+    #     if rhs[ 0 ] != '$':
+    #         for j in range( 1, len( rhs ) ):
+    #             print( i, j )
 
-
-    # for left_sym in nonterminals:
-    #     for rule in grammar[ left_sym ]:
-    #         calculateFirst( left_sym, rule[ 0 ], direct_first, grammar, set() )
-    #
-    # first = copy( direct_first )
-    #
-    # for left_sym, rules in grammar.items():
-    #     for rule in rules:
-    #         for symbol in rule:
-    #             if symbol[ 0 ] == '<':
-    #                 first[ left_sym ] |= direct_first[ symbol ]
-    #                 if symbol not in empty_symbols: break
-    #             else: break
-    #
-    # for symbol in empty_symbols:
-    #     first[ symbol ].add( '$' )
-    #
-    # for term in terminals:
-    #     first[ term ] = { term }
-    #
-    # print( direct_first )
-    # print( first )
-
-    # pickle.dump( {
-    #         'nonterminal'   : nonterminals,
-    #         'terminal'      : terminals,
-    #         'sync'          : syncs,
-    #         'grammar'       : grammar,
-    #         'first'         : first,
-    #         'empty_symbols' : empty_symbols
-    #     }, open( 'analizator/temp_defs.bin', 'wb' ) )
+    start_item = ( 0, 0, '#' )
+    print( start_item )
+    print( closure( { start_item } ) )
--- Lab2/zj/analizator/GenerateTable.py
+++
@@ -1,9 +0,0 @@
-# LR table generation from grammar and some extra info
-
-class MakeProductions:
-
-    def __init__( self ):
-        pass
-
-    def make_table( self ):
-        pass
